28e9c10f3f502ca5f6cad97ba0db9dd5cd207daf
ishansarvaiya <ishansarvaiya1@gmail.com>
2023-03-06 12:48:55 +0530

    Added server-side validations

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -20,5 +20,21 @@ namespace BulkyBookWeb.Controllers
         {
             return View();
         }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
     }
 }
