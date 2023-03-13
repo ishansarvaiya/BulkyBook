5ca23e27f6cf935dc42e031f2fb0ef761a8ce804
ishansarvaiya <ishansarvaiya1@gmail.com>
2023-03-13 12:43:51 +0530

    Added Repository and IRepository

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
